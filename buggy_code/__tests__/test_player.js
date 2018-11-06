0f11a54ef934026b029d3754dfa044dd437bd265
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2018-11-06 09:32:29 +0300

    subir proyecto

---
+++ buggy_code/__tests__/test_player.js
@@ -0,0 +1,80 @@
+//Importar ficheros
+const extractDataFromRawJSON = require('../app/data/json_reader').extractDataFromRawJSON;
+const readJSONData = require('../app/data/json_reader').readJSONData;
+let jsonObjects = readJSONData('./data/sample.json');
+let arrayPlayers = extractDataFromRawJSON(jsonObjects);
+
+//Tests
+test('TEAM TEST "PLAYER" FORWARD', () => {
+
+	expect(arrayPlayers[0].isForward()).toBe(true);
+	expect(arrayPlayers[1].isForward()).toBe(false);
+
+
+});
+
+test('TEAM TEST "PLAYER" GOALKEEPER', () => {
+
+	expect(arrayPlayers[0].isGoalKeeper()).toBe(false);
+
+});
+
+test('TEAM TEST "PLAYER" BACK', () => {
+
+	expect(arrayPlayers[0].isBack()).toBe(false);
+
+});
+
+test('TEAM TEST "PLAYER" MIDFIELDER', () => {
+
+	expect(arrayPlayers[0].isMidfielder()).toBe(true);
+
+});
+
+test('TEAM TEST "PLAYER" VALUE', () => {
+
+	expect(arrayPlayers[0].getValue()).toBe(95500000);
+
+});
+
+test('TEAM TEST "PLAYER" NATIONALITY', () => {
+
+	expect(arrayPlayers[0].getNationality()).toBe('Portugal');
+
+});
+
+test('TEAM TEST "PLAYER" TEAM', () => {
+
+	expect(arrayPlayers[0].getTeam()).toBe('Real Madrid CF');
+
+});
+
+test('TEAM TEST "PLAYER" AGE', () => {
+
+	expect(arrayPlayers[0].getAge()).toBe(32);
+
+});
+
+test('TEAM TEST "PLAYER" NAME', () => {
+
+	expect(arrayPlayers[0].getName()).toBe('Cristiano Ronaldo');
+
+});
+
+test('TEAM TEST "PLAYER" ID', () => {
+
+	expect(arrayPlayers[0].getID()).toBe(20801);
+
+});
+
+test('TEAM TEST "PLAYER" QUALITY', () => {
+
+	expect(arrayPlayers[0].getQuality()).toBe(94);
+
+});
+
+test('TEAM TEST "PLAYER" EQUALS', () => {
+
+	expect(arrayPlayers[0].equals(arrayPlayers[0])).toBe(true);
+
+});
---
+++ buggy_code/__tests__/test_team.js
@@ -0,0 +1,47 @@
+//Importar ficheros
+const extractDataFromRawJSON = require('../app/data/json_reader').extractDataFromRawJSON;
+const readJSONData = require('../app/data/json_reader').readJSONData;
+const Teams = require('../app/data/team').Team;
+let jsonObjects = readJSONData('./data/sample.json');
+let arrayPlayers = extractDataFromRawJSON(jsonObjects);
+
+let team1 = new Teams('team1');
+
+let cristiano = arrayPlayers[0];
+let messi = arrayPlayers[1];
+let neymar = arrayPlayers[2];
+team1.addPlayer(cristiano);
+team1.addPlayer(messi);
+//team1.addPlayer(neymar);
+//Tests
+test('TEAM TEST NAME', () => {
+
+    expect(team1.getTeamName()).toBe('team1');
+
+});
+test('TEAM TEST VALUE', () => {
+
+    expect(team1.getTeamValue()).toBe(200500000);
+
+});
+/*
+test('TEAM TEST PLAYERS',()=>{
+    expect(team1.getPlayers()).toBe('h');
+});*/
+test('TEAM TEST HAS_PLAYER', () => {
+
+    expect(team1.hasPlayer(neymar)).toBe(false);
+    expect(team1.hasPlayer(cristiano)).toBe(true);
+
+});
+test('TEAM TEST NUMBER OF PLAYERS', () => {
+
+    expect(team1.getNumberPlayers()).toBe(2);
+
+});
+
+test('TEAM TEST GET TEAM-QUALITY ', () => {
+
+    expect(team1.getTeamOverallQuality()).toBe(200500000);
+
+});
