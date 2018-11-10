d9198d60a28efc849d54e64d1b0c26774603984a
LA-BESTIA--PABL\pjord <pjorda96@gmail.com>
2018-11-10 20:04:38 +0100

    inicio test league

--- buggy_code/__tests__/test_team.js
+++ buggy_code/__tests__/test_team.js
@@ -14,68 +14,68 @@ team1.addPlayer(cristiano);
 team1.addPlayer(messi);
 
 let players = [{
-    "_isBack": false,
-    "_isForward": true,
-    "_isGoalKeeper": false,
-    "_isMidfielder": true,
-    "age": 32,
-    "club": "Real Madrid CF",
-    "country": "Portugal",
-    "id": 20801,
-    "name": "Cristiano Ronaldo",
-    "overall": 94,
-    "value": 95500000
+	"_isBack": false,
+	"_isForward": true,
+	"_isGoalKeeper": false,
+	"_isMidfielder": true,
+	"age": 32,
+	"club": "Real Madrid CF",
+	"country": "Portugal",
+	"id": 20801,
+	"name": "Cristiano Ronaldo",
+	"overall": 94,
+	"value": 95500000
 }, {
-    "_isBack": false,
-    "_isForward": false,
-    "_isGoalKeeper": false,
-    "_isMidfielder": true,
-    "age": 30,
-    "club": "FC Barcelona",
-    "country": "Argentina",
-    "id": 158023,
-    "name": "L. Messi",
-    "overall": 93,
-    "value": 105000000
+	"_isBack": false,
+	"_isForward": false,
+	"_isGoalKeeper": false,
+	"_isMidfielder": true,
+	"age": 30,
+	"club": "FC Barcelona",
+	"country": "Argentina",
+	"id": 158023,
+	"name": "L. Messi",
+	"overall": 93,
+	"value": 105000000
 }]
 
 //Tests
 test('TEAM TEST CONSTRUCTOR', () => {
 
-    let team2 = new Teams('test');
-    expect(team2.teamName).toBe('test');
-    expect(team2.listPlayers).toEqual([]);
-    expect(team2.teamValue).toBe(0);
+	let team2 = new Teams('test');
+	expect(team2.teamName).toBe('test');
+	expect(team2.listPlayers).toEqual([]);
+	expect(team2.teamValue).toBe(0);
 
 });
 
-test('TEAM TEST NAME',()=>{
+test('TEAM TEST NAME', () => {
 
-    expect(team1.getTeamName()).toBe('team1');
-
-});
-test('TEAM TEST VALUE',()=>{
-
-    expect(team1.getTeamValue()).toBe(200500000);
+	expect(team1.getTeamName()).toBe('team1');
 
 });
+test('TEAM TEST VALUE', () => {
 
-test('TEAM TEST PLAYERS',()=>{
-
-    expect(team1.getPlayers()).toEqual(players);
+	expect(team1.getTeamValue()).toBe(200500000);
 
 });
 
-test('TEAM TEST HAS_PLAYER',()=>{
+test('TEAM TEST PLAYERS', () => {
 
-    expect(team1.hasPlayer(neymar)).toBe(false);
-    expect(team1.hasPlayer(cristiano)).toBe(true);
+	expect(team1.getPlayers()).toEqual(players);
 
 });
 
-test('TEAM TEST NUMBER OF PLAYERS',()=>{
+test('TEAM TEST HAS_PLAYER', () => {
 
-    expect(team1.getNumberPlayers()).toBe(2);
+	expect(team1.hasPlayer(neymar)).toBe(false);
+	expect(team1.hasPlayer(cristiano)).toBe(true);
+
+});
+
+test('TEAM TEST NUMBER OF PLAYERS', () => {
+
+	expect(team1.getNumberPlayers()).toBe(2);
 
 });
 
@@ -96,8 +96,8 @@ addPlayer(player) {
 
 test('TEAM TEST ADD PLAYER', () => {
 
-    expect(team1.addPlayer(neymar)).toBe(true);
-    expect(f => addPlayer('./data/team.js')).toThrowError(Error);
+	expect(team1.addPlayer(neymar)).toBe(true);
+	expect(f => addPlayer('./data/team.js')).toThrowError(Error);
 
 });
 
@@ -107,9 +107,8 @@ test('TEAM TEST ADD PLAYER', () => {
 
 });*/
 
-test('TEAM TEST GET TEAM-QUALITY',()=>{
+test('TEAM TEST GET TEAM-QUALITY', () => {
 
-    expect(team1.getTeamOverallQuality()).toBe(93.5);
+	expect(team1.getTeamOverallQuality()).toBe(93.5);
 
 });
-
