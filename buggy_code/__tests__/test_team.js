d065a19b8a009f4416a492b7c9a2c3b272a1e723
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2018-11-10 12:06:10 +0300

    test team

--- buggy_code/__tests__/test_team.js
+++ buggy_code/__tests__/test_team.js
@@ -40,6 +40,15 @@ let players = [{
 }]
 
 //Tests
+test('TEAM TEST CONSTRUCTOR', () => {
+
+    let team2 = new Teams('test');
+    expect(team2.teamName).toBe('test');
+    expect(team2.listPlayers).toEqual([]);
+    expect(team2.teamValue).toBe(0);
+
+});
+
 test('TEAM TEST NAME',()=>{
 
     expect(team1.getTeamName()).toBe('team1');
@@ -63,13 +72,42 @@ test('TEAM TEST HAS_PLAYER',()=>{
     expect(team1.hasPlayer(cristiano)).toBe(true);
 
 });
+
 test('TEAM TEST NUMBER OF PLAYERS',()=>{
 
     expect(team1.getNumberPlayers()).toBe(2);
 
 });
 
-test('TEAM TEST GET TEAM-QUALITY ',()=>{
+/**
+ * Adds a new player to the team
+ * @param {Player} player The player to be added
+ * @returns {Boolean} true in case that the player was successfully 
+ * added to the team, false otherwise
+ *
+addPlayer(player) {
+    if (!this.hasPlayer(player)) {
+        this.listPlayers.push(player);
+        this.teamValue += player.getValue();
+        return true;
+    }
+    return false;
+}*/
+
+test('TEAM TEST ADD PLAYER', () => {
+
+    expect(team1.addPlayer(neymar)).toBe(true);
+    expect(f => addPlayer('./data/team.js')).toThrowError(Error);
+
+});
+
+/*test('TEAM TEST ADD PLAYERS', () => {
+
+    //expect(team1.getTeamOverallQuality()).toBe(93.5);
+
+});*/
+
+test('TEAM TEST GET TEAM-QUALITY',()=>{
 
     expect(team1.getTeamOverallQuality()).toBe(93.5);
 
