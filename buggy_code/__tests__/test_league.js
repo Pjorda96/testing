00f50ca5af946f169137b6ff23924547007c3332
LA-BESTIA--PABL\pjord <pjorda96@gmail.com>
2018-11-11 23:16:18 +0100

    league

--- buggy_code/__tests__/test_league.js
+++ buggy_code/__tests__/test_league.js
@@ -1,5 +1,9 @@
 //Importar ficheros
 const League = require('../app/data/league').League;
+const extractDataFromRawJSON = require('../app/data/json_reader').extractDataFromRawJSON;
+const readJSONData = require('../app/data/json_reader').readJSONData;
+let jsonObjects = readJSONData('./data/fifa_data.json');
+let arrayPlayers = extractDataFromRawJSON(jsonObjects);
 
 test('TEAM TEST CONSTRUCTOR', () => {
 	let league = new League(1);
@@ -11,17 +15,20 @@ test('TEAM TEST CONSTRUCTOR', () => {
 	//expect(f => new League('test')).toThrowError(Error);
 });
 
-/**
- * Method employed to add a team to the current league.
- * @param {Team} team The team to be added to the league
- * @throws {Error} In case that the league is already full
- *
-addTeam(team) {
-	if (this.listTeams.length == this.numberOfTeams) {
-		throw Error('The league is full with teams!');
-	}
-	this.listTeams.push(team);
-}*/
+test('TEAM TEST ADD TEAM', () => {
+	let league = new League(1);
+	league.addTeam('test');
+	expect(league.listTeams.length).toBe(1);
+	expect(league.listTeams[0]).toBe('test');
+
+	expect(f => league.addTeam('test')).toThrowError(Error);
+});
+
+test('TEAM TEST CREATES CALENDAR', () => { //TODO: terminar
+	let league = new League(1);
+	league.createCalendar();
+	expect(league.calendar).toEqual([]);
+});
 
 /*
  * TODO: Creates a calendar that determines when teams play against each other.
@@ -33,14 +40,30 @@ createCalendar() {
 	this.calendar = [];
 }*/
 
+test('TEAM TEST CREATES RANDOM LEAGUE', () => {
+	let league = League.createRandomLeague(arrayPlayers,3,1,10000000000000000);
+
+
+	//expect(league.listTeams.length).toBe(1);
+	//expect(league.listTeams[0]).toBe('test');
+
+	expect(f => League.createRandomLeague(arrayPlayers, 100, 1, 100000000)).toThrowError(Error);
+});
+
 /**
- * Creates a League object based on a pool of players, a number of teams to compete, and a minimum and maximum team value
- * @param {Array<Player>} playerData An array of player profiles that will be selected to generate the teams
- * @param {Number} numberOfTeams An integer that delimits the number of teams that will compete in the league
- * @param {Number} minTeamValue The minimum amount of euros that will be spent by clubs on forming the team
- * @param {Number} maxTeamValue The maximum amount of euros that will be spent by clubs on forming the team
+ * Creates a League object based on a pool of players, a number of teams to compete, 
+ * and a minimum and maximum team value
+ * @param {Array<Player>} playerData An array of player profiles that will be selected to 
+ * generate the teams
+ * @param {Number} numberOfTeams An integer that delimits the number of teams that will 
+ * compete in the league
+ * @param {Number} minTeamValue The minimum amount of euros that will be spent by clubs 
+ * on forming the team
+ * @param {Number} maxTeamValue The maximum amount of euros that will be spent by clubs 
+ * on forming the team
  * @returns {League} The league that has been created
- * @throws {Error} In case that there are not enough players to create the desired number of teams, or in case that the minimum team value is higher than the maximum team value 
+ * @throws {Error} In case that there are not enough players to create the desired number 
+ * of teams, or in case that the minimum team value is higher than the maximum team value 
  *
 static createRandomLeague(playerData, numberOfTeams, minTeamValue, maxTeamValue) {
 
